e743a7b7bd85e7b3bfdf821774a917c86c4aa019
ineedausernamehelp <vandekolkjake@gmail.com>
2024-03-06 15:52:23 +0100

    added documentation too the randomize method

--- Dealer.cs
+++ Dealer.cs
@@ -9,11 +9,12 @@
         public string[] shuffleCards(string [] deckOfCards)
         {
 
-
+            //Get random method from C#
              Random rng = new Random();
-
+            //Loop trough each item in deckOfCards
             for (int i = 0; i < deckOfCards.Length; i++)
             {
+                //Randomize the list of cards
                 int k = rng.Next(deckOfCards.Length);
                 string temp = deckOfCards[i];
                 deckOfCards[i] = deckOfCards[k];
